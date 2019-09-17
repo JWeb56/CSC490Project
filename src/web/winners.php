e159a0b5b0e05de3cc3ac2fe28957165b51ea8fd
Micah Roberts <m_rober4@uncg.edu>
2019-09-17 17:36:05 -0400

    Add basic HTML page

---
+++ src/web/winners.php
@@ -0,0 +1,51 @@
+<html>
+<style>
+    .topnav {
+        background-color: grey;
+        overflow: hidden;
+    }
+
+    /* Style the links inside the navigation bar */
+    .topnav a {
+        float: left;
+        margin-inline: 8%;
+        color: white;
+        text-align: center;
+        padding:14px  16px;
+        text-decoration: none;
+        font-size: 17px;
+    }
+
+    /* Change the color of links on hover */
+    .topnav a:hover {
+        background-color: black;
+        color: white;
+    }
+
+    /* Add a color to the active/current link */
+    .topnav a.active {
+        background-color: blue;
+        color: white;
+    }
+    body {
+        width: 960px;
+        margin: 0 auto;
+        padding: 0 25px;
+        font-family: arial, 'times new roman', sans-serif;
+        font-size: 1em;
+        text-align: justify;
+    }
+
+</style>
+<head>
+
+    <div class="topnav">
+        <a href="test.php">Home</a>
+        <a href="judge.php">Judge</a>
+        <a class="active" href="winners.php">Winners</a>
+        <a href="settings.php">Settings</a>
+    </div>
+</head>
+<body>
+</body>
+</html>
