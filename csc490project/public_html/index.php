8ae2287f311968c3d3443a8aff88d02620efdabc
John Weber <jsweber2@uncg.edu>
2019-09-17 19:05:48 -0400

    progress 9/17

--- csc490project/public_html/index.php
+++ csc490project/public_html/index.php
@@ -1,1 +1,35 @@
-<?php echo "Hello World";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="CSC490 Project Grading Application">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <title>CSC490 Project Grading Application</title>
+</head>
+<body>
+<header>
+    <nav class="nav-header-main">
+        <a href="#">
+            <img src="link/to/logo.jpg" alt="logo">
+        </a>
+        <ul>
+            <li><a href="login.php">Home</a> </li>
+            <li><a href="#">Poster Judging</a> </li>
+            <li><a href="../view/home/signup.php">Create Account</a> </li>
+            <li><a href="#">Info</a> </li>
+        </ul>
+        <div>
+            <form action="login.php" method="post">
+                <input type="text" name="login_input" placeholder="Username/Email">
+                <input type="password" name="pwd" placeholder="Password">
+                <button type="submit" name="login-submit">Login</button>
+            </form>
+            <a href="signup.php">Sign Up</a>
+            <form action="logout.php" method="post">
+                <button type="submit" name="logout-submit">Logout</button>
+            </form>
+        </div>
+    </nav>
+</header>
+</body>
+</html>
---
+++ csc490project/view/home/signup.php
@@ -0,0 +1,9 @@
+
+
+<h1>Create Account</h1>
+<form action="signup.php" method="post">
+    <input type="text" name="user_name" placeholder="Username">
+    <input type="password" name="pwd" placeholder="Password">
+    <input type="password" name="pwd-repeat" placeholder="Re-type password">
+    <button type="submit" name="signup-submit">Create Account</button>
+</form>
