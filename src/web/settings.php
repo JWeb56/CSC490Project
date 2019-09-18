1e3f9ac041657b6f0b5c9192775fd632e01f69c0
Micah Roberts <m_rober4@uncg.edu>
2019-09-18 15:51:03 -0400

    Added functionallity for all pages so they all look standard.

--- src/web/settings.php
+++ src/web/settings.php
@@ -1,51 +1,62 @@
-<html>
-<style>
-    .topnav {
-        background-color: grey;
-        overflow: hidden;
-    }
-
-    /* Style the links inside the navigation bar */
-    .topnav a {
-        float: left;
-        margin-inline: 8%;
-        color: white;
-        text-align: center;
-        padding:14px  16px;
-        text-decoration: none;
-        font-size: 17px;
-    }
-
-    /* Change the color of links on hover */
-    .topnav a:hover {
-        background-color: black;
-        color: white;
-    }
-
-    /* Add a color to the active/current link */
-    .topnav a.active {
-        background-color: blue;
-        color: white;
-    }
-    body {
-        width: 960px;
-        margin: 0 auto;
-        padding: 0 25px;
-        font-family: arial, 'times new roman', sans-serif;
-        font-size: 1em;
-        text-align: justify;
-    }
-
-</style>
+<!doctype html>
+<html lang="en">
 <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <div class="topnav">
-        <a href="test.php">Home</a>
-        <a href="judge.php">Judge</a>
-        <a href="winners.php">Winners</a>
-        <a class="active" href="settings.php">Settings</a>
-    </div>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        .navbar-nav.navbar-center {
+            position: absolute;
+            left: 50%;
+            transform: translatex(-50%);
+        }
+        .navbar-nav > li{
+            padding-left:70px;
+            padding-right:70px;
+        }
+    </style>
+    <title>Judge Me App</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <a class="navbar-brand" href="home2.html">JMapp</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="nav navbar-nav navbar-center">
+
+            <li class="nav-item">
+                <a class="nav-link" href="home.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="judge.php">Judge</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="winners.php">Winners</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="settings.php">Settings<span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="display-1">User Settings</h1>
+    </div>
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
