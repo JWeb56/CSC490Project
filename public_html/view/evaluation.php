39554e351ccdf171082688b7c8c557647a943011
JWeb56 <JWeb56@users.noreply.github.com>
2019-12-06 02:07:06 -0500

    Functional grading

--- public_html/view/evaluation.php
+++ public_html/view/evaluation.php
@@ -44,9 +44,9 @@ function adder() {
                         </a>
                     </li>
                     <li class="nav-item ">
-                        <a id="l2" class="nav-link" href="judge.php">
-                            <i id="icon2" class="material-icons">assignment</i>
-                            <p id="p2">Judge Events</p>
+                        <a id="l2" class="nav-link" href="Profile.php">
+                            <i id="icon2" class="material-icons">person</i>
+                            <p id="p2">Profile</p>
                         </a>
                     </li>
                     <li class="nav-item active ">
