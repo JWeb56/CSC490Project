9807d5796d1f63096f425cb4db81a00ac859a15a
JWeb56 <JWeb56@users.noreply.github.com>
2019-12-05 23:20:34 -0500

    Add results page

---
+++ public_html/admin/results.php
@@ -0,0 +1,62 @@
+<?php
+include('includes/header.php');
+include('includes/navbar.php');
+require '../includes/db.inc.php.template';
+$sql = "select name, session_id, AVG(total_score) as total from participant ORDER BY session_id";
+$stmt = mysqli_stmt_init($connection);
+mysqli_stmt_prepare($stmt, $sql);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$results = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    array_push($results, $row);
+}
+?>
+
+
+
+
+
+
+
+    <div class="container-fluid">
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+
+
+            <div class="card-body">
+
+                <div class="table-responsive">
+
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th>Participant</th>
+                            <th>Session</th>
+                            <th>Total</th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        <?php foreach ($results as $entry) { ?> <tr>
+                            <td> <?php echo $entry['name'] ?> </td>
+                            <td> <?php echo $entry['session_id'] ?> </td>
+                            <td> <?php echo $entry['total'] ?> </td>
+
+                        </tr> <?php } ?>
+
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+<?php
+include('includes/footer.php');
+?>
